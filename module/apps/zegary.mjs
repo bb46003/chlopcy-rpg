5c1cb726a551a4e25e4b49127436e9e681259801
BartlomiejBielecki <bartlomiejbielecki@gmail.com>
2025-01-26 17:04:10 +0100

    finiszh manual tykacz update
    manualnie zmniejszanie czasu i osiągów Zagara Tykacza

--- module/apps/zegary.mjs
+++ module/apps/zegary.mjs
@@ -8,7 +8,7 @@ export class zegarTykacza extends Application {
 
 
     // Initialize instance data
-    const osizagiZegar = (tykacz?.system.pozostaleOsiagi > 0) 
+    const osiagiZegar = (tykacz?.system.pozostaleOsiagi > 0) 
     ? tykacz?.system.pozostaleOsiagi 
     : tykacz?.system.osiagi;
     const czasZegar = (tykacz?.system.pozostalyCzas > 0)
@@ -16,7 +16,7 @@ export class zegarTykacza extends Application {
     : tykacz?.system.czasTrwania
     this.data = {
       tykacz,
-      osizagiZegar,
+      osiagiZegar,
       czasZegar,
     };
 
@@ -57,7 +57,8 @@ export class zegarTykacza extends Application {
     html.on("click", ".nazwa-zegar i.fas.fa-window-close", (ev) =>
       this.closeApp(ev)
     );
-    html.on("click",".osiagi i.fa.fa-minus", (ev)=>this.zmiejszOsiagi(ev))
+    html.on("click",".osiagi i.fa.fa-minus.osiagi", (ev)=>this.zmiejszOsiagi(ev))
+    html.on("click",".czas-trwania i.fa.fa-minus.czas", (ev)=>this.zmiejszCzas(ev))
   }
 
   async closeApp(ev) {
@@ -73,19 +74,43 @@ export class zegarTykacza extends Application {
   }
   async zmiejszOsiagi(ev){
     const obecnyTykacz = this;
-    const obecnieOsiagi = this.data.osizagiZegar;
+    const obecnieOsiagi = this.data.osiagiZegar;
     const tykacz = obecnyTykacz.data.tykacz;
     const noweOsiagi = obecnieOsiagi -1;
-    obecnyTykacz.data.osizagiZegar = noweOsiagi;
+    obecnyTykacz.data.osiagiZegar = noweOsiagi;
     tykacz.update({["system.pozostaleOsiagi"]:noweOsiagi});
     const container = obecnyTykacz.element; 
     if (container) {
       container.find(".osiagi-input").val(noweOsiagi);
     }
-    if(noweOsiagi === 1){
+    if(noweOsiagi === 0){
       this.closeApp(ev)
     }
-    
+    game.socket.emit("system.chlopcy", {
+      type: "zmniejszOsiagiZegara",
+      noweOsiagi: noweOsiagi,
+      tykacz: this.data?.tykacz,
+    });    
+  }
+  async zmiejszCzas(ev){
+    const obecnyTykacz = this;
+    const obecnyCzas = this.data.czasZegar;
+    const tykacz = obecnyTykacz.data.tykacz;
+    const nowyCzas = obecnyCzas -1;
+    obecnyTykacz.data.czasZegar = nowyCzas;
+    tykacz.update({["system.pozostalyCzas"]:nowyCzas});
+    const container = obecnyTykacz.element; 
+    if (container) {
+      container.find(".czas-trwania-input").val(nowyCzas);
+    }
+    if(nowyCzas === 0){
+      this.closeApp(ev)
+    }
+    game.socket.emit("system.chlopcy", {
+      type: "zmniejszCzasZegara",
+      nowyCzas: nowyCzas,
+      tykacz: this.data?.tykacz,
+    });    
   }
 }
 
@@ -96,28 +121,48 @@ export class zegarTykaczaSocketHandler{
   }
   registerSocketEvents() {
     game.socket.on("system.chlopcy", async (data) => {
-      if (data.type === "renderZegarTykacza") {
-        const actor = data.actor;
-        if (actor) {
-          zegarTykacza.initialise(actor);
-        }
-      }
-    });
-    game.socket.on("system.chlopcy", (data)=>{
-      if (data.type === "zamknijZegarTykacza") {
-        const tykaczArray = Array.from(game.chlopcy.zegarTykacza.instances.values()); 
-        const tykacz = data.tykacz
-        const zegar = tykaczArray.find((element) => element.data.tykacz._id === tykacz._id); // Find the matching element
-        
-        if (zegar) {
-          
-          zegar.close(); // Close the found instance
-        } else {
-        
-        }
+      let tykacz, zegar,container;
+      const tykaczArray = Array.from(game.chlopcy.zegarTykacza.instances.values()); 
+      switch(data.type){
+        case "renderZegarTykacza":
+          const actor = data.actor;
+          if (actor) {
+            zegarTykacza.initialise(actor);
+          }
+        break;
+    
+        case "zamknijZegarTykacza":
+      
+          tykacz = data.tykacz
+          zegar = tykaczArray.find((element) => element.data.tykacz._id === tykacz._id);         
+          if (zegar) {          
+            zegar.close(); 
+          }     
+        break;
+
+        case "zmniejszOsiagiZegara":
+              const noweOsiagi = data.noweOsiagi;
+              tykacz = data.tykacz
+              zegar = tykaczArray.find((element) => element.data.tykacz._id === tykacz._id);
+              container = zegar.element; 
+              if (container) {
+                container.find(".osiagi-input").val(noweOsiagi);
+              }
+        break;
+
+        case "zmniejszCzasZegara":
+          const nowyCzas = data.nowyCzas;
+          tykacz = data.tykacz
+          zegar = tykaczArray.find((element) => element.data.tykacz._id === tykacz._id);
+          container = zegar.element; 
+              if (container) {
+                container.find(".czas-trwania-input").val(nowyCzas);
+              }
+
       }
       
 
     })
+    
   }
 }
