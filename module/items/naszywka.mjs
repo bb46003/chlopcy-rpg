cff0f6b2ace303ce9745490e4851cf16a1cc05e5
BartlomiejBielecki <bartlomiejbielecki@gmail.com>
2025-06-20 14:27:41 +0200

    tykacz działa na appv2

--- module/items/naszywka.mjs
+++ module/items/naszywka.mjs
@@ -1,13 +1,10 @@
-const BaseItemSheet =
-  typeof foundry?.appv1?.sheets?.ItemSheet !== "undefined"
-    ? foundry.appv1.sheets.ItemSheet
-    : ItemSheet;
+const BaseItemSheet = typeof foundry?.appv1?.sheets?.ItemSheet !== 'undefined' ? foundry.appv1.sheets.ItemSheet : ItemSheet;
 
 export class naszywka extends BaseItemSheet {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["naszywki_przody"],
-      template: "systems/chlopcy/tameplates/item/naszywka.hbs",
+      classes: ['naszywki_przody'],
+      template: 'systems/chlopcy/tameplates/item/naszywka.hbs',
       width: 500,
       height: 400,
     });
